7c6f69f8e2fd0a6594ffb9552d9d6584f7cce93d
Vincent_Li <q315744068@gmail.com>
2024-09-23 20:23:28 +0800

    UPDATE

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -15,8 +15,9 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
-    }
+        Some(first) => first.to_string().to_uppercase(),
+    };
+    input.to_string()
 }
 
 // Step 2.
@@ -24,7 +25,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut ret = vec![];
+    for word in words.iter() {
+        ret.push(capitalize_first(word));
+    }
+    ret
 }
 
 // Step 3.
@@ -32,7 +37,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.iter().map(|word| capitalize_first(word)).collect::<Vec<_>>().join(" ")
 }
 
 #[cfg(test)]
