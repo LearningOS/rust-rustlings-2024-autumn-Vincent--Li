3c2eae4efc5c8a2c0093c180dfe504dedda589a8
vincent <vincent@Vincent>
2024-09-24 23:06:25 +0800

    UPDATE

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut iter = s.split(",");
+        match (iter.next(), iter.next(), iter.next()) {
+            (Some(""), _, _) => Person::default(),
+            (Some(name), Some(age), None) => {
+                match age.parse::<usize>() {
+                    Ok(age) => Person { name: name.to_string(), age},
+                    Err(_) => Person::default(), 
+                }
+            },
+            _ => Person::default(),
+        }
     }
 }
 
